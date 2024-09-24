2c24a77f5c0bf94a6855533a12d0982b3c9a2be5
Brian Leonardo de Morais <morais.brian360@gmail.com>
2024-09-24 12:33:14 -0300

    Alteração de portas de aplicações em manifestos k8s

--- MedicalConsultation.Patients.Api/src/app.ts
+++ MedicalConsultation.Patients.Api/src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
+import os from 'os';
 import { container, Setup } from './ioc/setup';
 import { Server } from './api/server';
 
@@ -9,4 +10,4 @@ Setup.configure();
 const server = container.resolve(Server);
 
 const port = process.env.PORT || 3000;
-server.app.listen(port, () => console.log('Server On'));
+server.app.listen(port, () => console.log(`Server On: ${os.hostname()}:${process.env.PORT}`));
